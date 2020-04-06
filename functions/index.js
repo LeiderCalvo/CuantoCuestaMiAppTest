9eed5d3daa239ba78883f9c86b99d54fc25dc662
LeiderCalvo <leiderstivencalvogomez@hotmail.com>
2020-04-06 16:42:34 -0500

    re-structuring the moment to send the emails

--- functions/index.js
+++ functions/index.js
@@ -74,9 +74,9 @@ exports.sendEmails = functions.https.onRequest((req, res) => {
 
     const mailOptions = {
         from: `firebaseGCE@gmail.com`,
-        to: 'firebaseGCE@gmail.com',
-        subject: 'Quote Completed',
-        html: `<h1>Order Completed</h1>
+        to: `firebaseGCE@gmail.com,${obj[0]}`,
+        subject: 'Gracias por confiar en nosotros',
+        html: `<h1>Orden completada</h1>
                 <p> <b>Email: </b>${obj[0]}</p>
                 <p> <b>Answer1: ¿Qué tipo de web estás buscando? - </b>${obj[1]}</p>
                 <p> <b>Answer2: ¿Qué diseño quieres que tenga tu Web? - </b>${obj[2]}</p>
@@ -86,7 +86,8 @@ exports.sendEmails = functions.https.onRequest((req, res) => {
                 <p> <b>Answer6: ¿Tu web necesita alguno de los siguientes? - </b>${obj[6][0]}, ${obj[6][1]}, ${obj[6][2]}</p>
                 <p> <b>Answer7: ¿Tu web necesita servicios de SEO? - </b>${obj[7]}</p>
                 <p> <b>Price: </b>${obj[8]} €</p>
-                <p> <b>Plan: </b>${obj[9]}</p>`
+                <p> <b>Plan: </b>${obj[9]}</p>
+                <h2>Entremos en contacto: https://goconsultingeurope.com/start-selector/</h2>`
     };
     return transporter.sendMail(mailOptions, (error, data) => {
         if (error) {
@@ -97,37 +98,10 @@ exports.sendEmails = functions.https.onRequest((req, res) => {
     });
 });
 
-exports.sendEmailToCostumer = functions.https.onRequest((req, res) => {
-
-    const obj = JSON.parse(req.body.content);
-    let id = obj[0].replace('@',''); id = id.replace('.','');
+exports.setUserActionBuy = functions.https.onRequest((req, res) => {
+    const obj = JSON.parse(req.body.email);
+    let id = obj.replace('@',''); id = id.replace('.','');
     
     admin.firestore().collection('requests').doc(`${id}`).update({action: true}).then(r => res.send('completed') 
     ).catch(err=> res.send(err) );
-
-    const mailOptions = {
-        from: `firebaseGCE@gmail.com`,
-        to: obj[0],
-        subject: 'Crear proyecto con GoConsultingEurope',
-        html: `<h1>Orden recibida</h1>
-                <p> <b>Tu correo: </b>${obj[0]}</p>
-                <p> <b>Respuesta 1: ¿Qué tipo de web estás buscando? - </b>${obj[1]}</p>
-                <p> <b>Respuesta 2: ¿Qué diseño quieres que tenga tu Web? - </b>${obj[2]}</p>
-                <p> <b>Respuesta 3: ¿En qué etapa se encuentra tu web? - </b>${obj[3]}</p>
-                <p> <b>Respuesta 4: ¿Qué tanta información necesita tu web? - </b> paginas: ${obj[4][0]}, productos: ${obj[4][1]}, idiomas:${obj[4][2]}</p>
-                <p> <b>Respuesta 5: ¿Necesitas ayuda en la creación de textos o imágenes? - </b>${obj[5][0]}, ${obj[5][1]}, ${obj[5][2]}</p>
-                <p> <b>Respuesta 6: ¿Tu web necesita alguno de los siguientes? - </b>${obj[6][0]}, ${obj[6][1]}, ${obj[6][2]}</p>
-                <p> <b>Respuesta 7: ¿Tu web necesita servicios de SEO? - </b>${obj[7]}</p>
-                <p> <b>Price: </b>${obj[8]} €</p>
-                <p> <b>Plan: </b>${obj[9]}</p>
-                
-                <h2>Entremos en contacto: https://goconsultingeurope.com/start-selector/</h2>`
-    };
-    return transporter.sendMail(mailOptions, (error, data) => {
-        if (error) {
-            res.send(error.toString());
-            return
-        }
-        return res.send('Sended5');
-    });
 });
